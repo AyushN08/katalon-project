f7e5b468605e32c9ead1e2d2e6091162473f8f11
Ayush <nayak.ap@somaiya.edu>
2025-09-25 23:35:51 +0530

    Added test cases for implementation

--- Scripts/TC_OpenExample/Script1758822347710.groovy
+++ Scripts/TC_OpenExample/Script1758822347710.groovy
@@ -19,6 +19,37 @@ import org.openqa.selenium.Keys as Keys
 
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+// 1. Open browser
+WebUI.openBrowser('')
+
+// 2. Navigate to Example.com
+WebUI.navigateToUrl('https://example.com')
+
+// 3. Verify page text
+WebUI.verifyTextPresent('Example Domain', false)
+
+// 4. Highlight the heading (h1 text "Example Domain")
+TestObject heading = new TestObject('heading')
+heading.addProperty('xpath', com.kms.katalon.core.testobject.ConditionType.EQUALS, '//h1')
+WebUI.verifyElementPresent(heading, 5)
+
+// 5. Get the text of heading and log it
+String headingText = WebUI.getText(heading)
+WebUI.comment("Heading text is: " + headingText)
+
+// 6. Verify paragraph text exists
+TestObject para = new TestObject('paragraph')
+para.addProperty('xpath', com.kms.katalon.core.testobject.ConditionType.EQUALS, '//p')
+WebUI.verifyElementPresent(para, 5)
+String paraText = WebUI.getText(para)
+WebUI.comment("Paragraph text is: " + paraText)
+
+// 7. Take a screenshot
+WebUI.takeScreenshot('Reports/example_page.png')
+
+// 8. Close browser
+WebUI.closeBrowser()
+
 // Demo: open example.com and assert text is present
 WebUI.openBrowser('')
 WebUI.navigateToUrl('https://example.com')
