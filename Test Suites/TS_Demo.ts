ea2b625e8b0753dcf39ec8a33ffd49aed71a96c4
Ayush <nayak.ap@somaiya.edu>
2025-09-25 23:19:15 +0530

    Add demo test TC_OpenExample and TS_Demo

---
+++ Test Suites/TS_Demo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8d91a4dc-a263-468e-8d75-28fb9df0e372</testSuiteGuid>
+   <testCaseLink>
+      <guid>e119536a-84c4-441c-8393-7b0d2f4bcaf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_OpenExample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
